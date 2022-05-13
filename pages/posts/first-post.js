eafea9d25c5859f00d140d530c203d5ab64363ff
ajinajink <105297115+ajinajink@users.noreply.github.com>
2022-05-13 10:54:55 +0900

    Update first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,1 +1,18 @@
+import Link from 'next/link';
 
+export default function FirstPost() {
+    return (
+        <>
+
+            <h1><center>My Personal infomration</center></h1>
+            <p><center>Name : Jina K.</center></p>
+            <p><center>Gender : F</center></p>
+            <p><center>MBTI : INFJ</center></p>
+            <h2>
+                <Link href="/">
+                    <a><center>Back to home</center></a>
+                </Link>
+            </h2>
+        </>
+    );
+} 
